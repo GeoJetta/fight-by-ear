0ad38900694afac597e892cac20228f00658f61d
Guest User <Guest@902-11.lmusd.org>
2018-03-01 12:34:06 -0800

    First

---
+++ Assets/CamCast.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamCast : MonoBehaviour {
+	public Camera cam;
+	public Transform prefab;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			RaycastHit hit;
+			Ray ray = cam.ScreenPointToRay (Input.mousePosition);
+			if (Physics.Raycast (ray, out hit)) {
+				Transform objectHit = hit.transform;
+				if (hit.collider.gameObject.tag == "Enemy" && hit.distance < 5.0f) {
+					Instantiate(prefab, new Vector3(Random.Range(-10,10), 2, Random.Range(-10,10)), Quaternion.identity);
+					Destroy (hit.collider.gameObject);
+				}
+			}
+
+		}
+	}
+}
