c5e8795daf41cfa2941cd84da632392134e6b707
Guest User <Guest@902-11.lmusd.org>
2018-03-08 11:10:35 -0800

    didn't get to upload this last time

--- Assets/CamCast.cs
+++ Assets/CamCast.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class CamCast : MonoBehaviour {
 	public Camera cam;
 	public Transform prefab;
+	Light myLight;
+
 	// Use this for initialization
 	void Start () {
-		
+		myLight = (Light)GameObject.FindObjectOfType (typeof(Light));
+		myLight.enabled = false;
 	}
 	
 	// Update is called once per frame
@@ -15,14 +18,24 @@ public class CamCast : MonoBehaviour {
 		if (Input.GetMouseButtonDown (0)) {
 			RaycastHit hit;
 			Ray ray = cam.ScreenPointToRay (Input.mousePosition);
+			Debug.DrawRay(transform.position, transform.forward, Color.green);
 			if (Physics.Raycast (ray, out hit)) {
 				Transform objectHit = hit.transform;
 				if (hit.collider.gameObject.tag == "Enemy" && hit.distance < 5.0f) {
-					Instantiate(prefab, new Vector3(Random.Range(-10,10), 2, Random.Range(-10,10)), Quaternion.identity);
-					Destroy (hit.collider.gameObject);
+					StartCoroutine (Example(hit));
 				}
 			}
 
 		}
 	}
+
+	IEnumerator Example( RaycastHit iHit )
+	{
+		myLight.enabled = true;
+		yield return new WaitForSeconds(2);
+		myLight.enabled = false;
+		Instantiate(prefab, new Vector3(Random.Range(-10,10), 2, Random.Range(-10,10)), Quaternion.identity);
+		Destroy (iHit.collider.gameObject);
+	}
+
 }
--- Assets/enemy.cs
+++ Assets/enemy.cs
@@ -5,15 +5,39 @@ using UnityEngine;
 public class enemy : MonoBehaviour {
 
 	public GameObject player;
+	Renderer renderer;
+	Material material;
+	AudioSource audioSource;
 
 	// Use this for initialization
 	void Start () {
+		renderer = GetComponent<Renderer> ();
 		player = GameObject.FindGameObjectWithTag ("Player");
+		material = renderer.material;
+		material.DisableKeyword("_EMISSION");
+		audioSource.time = Random.Range (0.0f, 40.0f);
+		audioSource.Play ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.position = Vector3.Lerp(transform.position,player.transform.position,Time.deltaTime*0.2f);
+		if (Vector3.Distance (transform.position, player.transform.position) < 1.5f)
+			LightOn (false);
+		else if (Vector3.Distance (transform.position, player.transform.position) < 2.0f)
+			LightOn (true); 
+		else
+			LightOn (false);
+	}
+
+	public void LightOn( bool on )
+	{
+
+		if( on )
+			material.EnableKeyword("_EMISSION");
+		else
+			material.DisableKeyword("_EMISSION");
+
 	}
 		
 }
