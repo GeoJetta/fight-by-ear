e53ed7736c874cf0c079ebcae9e9be7ce0e586d5
Guest User <Guest@902-11.lmusd.org>
2018-03-08 11:42:01 -0800

    new

--- Assets/CamCast.cs
+++ Assets/CamCast.cs
@@ -6,23 +6,32 @@ public class CamCast : MonoBehaviour {
 	public Camera cam;
 	public Transform prefab;
 	Light myLight;
+	public AudioSource death;
+	public AudioSource shot;
+	public AudioClip shoot;
 
 	// Use this for initialization
 	void Start () {
 		myLight = (Light)GameObject.FindObjectOfType (typeof(Light));
 		myLight.enabled = false;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButtonDown (0)) {
+			shot.PlayOneShot(shoot);
 			RaycastHit hit;
 			Ray ray = cam.ScreenPointToRay (Input.mousePosition);
 			Debug.DrawRay(transform.position, transform.forward, Color.green);
 			if (Physics.Raycast (ray, out hit)) {
 				Transform objectHit = hit.transform;
 				if (hit.collider.gameObject.tag == "Enemy" && hit.distance < 5.0f) {
-					StartCoroutine (Example(hit));
+					myLight.enabled = true;
+					Instantiate(prefab, new Vector3(Random.Range(-10,10), 2, Random.Range(-10,10)), Quaternion.identity);
+					Destroy (hit.collider.gameObject);
+					myLight.enabled = false;
+					death.Play();
 				}
 			}
 
